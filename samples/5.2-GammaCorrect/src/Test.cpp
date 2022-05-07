1e8ad6dd7a3ecd937bb2bfe7eed4f73c810daeb7
wangqingping <1214168935@qq.com>
2022-05-07 17:31:13 +0800

    fix a bug about plan data

--- samples/5.2-GammaCorrect/src/Test.cpp
+++ samples/5.2-GammaCorrect/src/Test.cpp
@@ -213,7 +213,7 @@ void rendPlan()
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
 		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
 		glEnableVertexAttribArray(2);
 		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
 		glBindVertexArray(0);
